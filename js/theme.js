fc802a9e4760dd64597f1f37a077312cc8d08b97
Benzazoua <billalben252@gmail.com>
2024-03-20 18:06:04 +0100

    Light & dark mode

--- js/theme.js
+++ js/theme.js
@@ -1,1 +1,24 @@
 "use strict";
+
+const /** {HTMLElement} */ $HTML = document.documentElement;
+
+let /** {Bool} */ isDark = matchMedia("(prefers-color-scheme: dark)").matches;
+
+if (sessionStorage.getItem("theme")) {
+  $HTML.dataset.theme = sessionStorage.getItem("theme");
+} else {
+  $HTML.dataset.theme = isDark ? "dark" : "light";
+}
+
+const changeTheme = () => {
+  isDark = !isDark;
+  sessionStorage.setItem("theme", isDark ? "light" : "dark");
+
+  // $HTML.dataset.theme = isDark ? "light" : "dark";
+  $HTML.dataset.theme = sessionStorage.getItem("theme");
+};
+
+window.addEventListener("load", () => {
+  const $themeBtn = document.querySelector("[data-theme-toggler]");
+  $themeBtn.addEventListener("click", changeTheme);
+});
