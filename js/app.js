818b040e14248ce0e1ecf962adccb134cd5b833c
Benzazoua <billalben252@gmail.com>
2024-03-25 22:13:01 +0100

    Fetch and render featured photos

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 "use strict";
+
+// Import
+import { client } from "./api_configure.js";
+import { photoCard } from "./photo_card.js";
+
+// Render curated photos in home page
+const $photoGrid = document.querySelector("[data-photo-grid]");
+$photoGrid.innerHTML = `<div class="skeleton"></div>`.repeat(18);
+
+client.photos.curated({ page: 1, per_page: 10 }, (data) => {
+  $photoGrid.innerHTML = "";
+
+  data.photos.forEach((photo) => {
+    const $photoCard = photoCard(photo);
+    $photoGrid.appendChild($photoCard);
+  });
+});
--- js/photo_card.js
+++ js/photo_card.js
@@ -1,1 +1,74 @@
 "use strict";
+
+// Import
+import { ripple } from "./utils/ripple.js";
+
+/**
+ * 
+ * @param {object} photo - object from API
+ * @returns {HTMLDivElement} card div
+ */
+
+export const photoCard = (photo) => {
+  const root = window.location.origin;
+  const {
+    alt,
+    avg_color: backdropColor,
+    width,
+    height,
+    id,
+    src: { large },
+  } = photo;
+
+  const $card = document.createElement("div");
+  $card.classList.add("card", "grid-item");
+  $card.style.backgroundColor = backdropColor;
+
+  $card.innerHTML = `
+    <figure class="card-banner" style="--width: ${width}; --height: ${height}">
+      <img
+        src="${large}"
+        width="${width}"
+        height="${height}"
+        alt="${alt}"
+        loading="lazy"
+        class="img-cover"
+      />
+    </figure>
+    <div class="card-content">
+      <button
+        class="icon-btn small"
+        aria-label="Add to favorite"
+        data-ripple
+        data-toggler-btn
+      >
+        <span class="material-symbols-outlined" aria-hidden="true">
+          favorite
+        </span>
+        <div class="state-layer"></div>
+      </button>
+    </div>
+    <a
+      href="${root}/pages/photos/photo_detail.html?id=${id}"
+      class="state-layer"
+    ></a>
+  `;
+
+  const $cardBanner = $card.querySelector("img");
+  $cardBanner.style.opacity = 0;
+
+  $cardBanner.addEventListener("load", function () {
+    this.animate(
+      {
+        opacity: 1,
+      },
+      { duration: 400, fill: "forwards" }
+    );
+  });
+
+  const $rippleElements = [$card, ...$card.querySelectorAll("[data-ripple]")];
+  console.log($rippleElements);
+  $rippleElements.forEach(($rippleElement) => ripple($rippleElement));
+
+  return $card;
+};
