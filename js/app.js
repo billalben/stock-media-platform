f5cacee87371a231b6780a443bebc7750c7f7b8e
Benzazoua <billalben252@gmail.com>
2024-03-26 18:40:50 +0100

    Fetch and render popular videos

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@
 import { client } from "./api_configure.js";
 import { photoCard } from "./photo_card.js";
 import { gridInit, updateGrid } from "./utils/masonry_grid.js";
+import { videoCard } from "./video_card.js";
 
 // Render curated photos in home page
 const $photoGrid = document.querySelector("[data-photo-grid]");
@@ -19,5 +20,22 @@ client.photos.curated({ page: 1, per_page: 10 }, (data) => {
 
     // $photoGrid.appendChild($photoCard);
     updateGrid($photoCard, photoGrid.$columnsHeight, photoGrid.$columns);
+  });
+});
+
+// Render popular videos in home page
+const $videoGrid = document.querySelector("[data-video-grid]");
+$videoGrid.innerHTML = `<div class="skeleton"></div>`.repeat(18);
+
+client.videos.popular({ per_page: 10 }, (data) => {
+  $videoGrid.innerHTML = "";
+
+  const videoGrid = gridInit($videoGrid);
+
+  data.videos.forEach((video) => {
+    const $videoCard = videoCard(video);
+
+    // $videoGrid.appendChild($videoCard);
+    updateGrid($videoCard, videoGrid.$columnsHeight, videoGrid.$columns);
   });
 });
--- js/video_card.js
+++ js/video_card.js
@@ -1,1 +1,74 @@
 "use strict";
+
+// Import
+import { ripple } from "./utils/ripple.js";
+import { favorite } from "./favorite.js";
+import { hoverOnPlay } from "./utils/hoverOnPlay.js";
+
+/**
+ * Create a video card
+ * @param {object} video - Video object
+ * @returns {HTMLElement} - Video card element
+ */
+
+export const videoCard = (video) => {
+  const root = window.location.origin;
+  const { width, height, id, image, video_files } = video;
+
+  const /** {Object} */ sdVideo = video_files.find(
+      (item) => item.quality === "sd" && item.width < 1000
+    );
+  const { file_type, link } = sdVideo;
+
+  const $card = document.createElement("div");
+  $card.classList.add("card", "grid-item", "video");
+
+  const favoriteObj = JSON.parse(localStorage.getItem("favorites"));
+
+  $card.innerHTML = `
+    <div class="card-banner" style="--width: ${width}; --height: ${height}">
+      <video
+        poster="${image}"
+        muted
+        loop
+        preload="none"
+        class="img-cover"
+        data-video
+      >
+        <source src="${link}" type="${file_type}" />
+      </video>
+    </div>
+    <div class="card-content">
+      <button
+        class="icon-btn small ${favoriteObj.videos[id] ? "active" : ""}"
+        aria-label="Add to favorite"
+        data-ripple
+        data-favorite-btn
+      >
+        <span class="material-symbols-outlined" aria-hidden="true">
+          favorite
+        </span>
+        <div class="state-layer"></div>
+      </button>
+    </div>
+    <div class="card-badge" data-card-badge>
+      <span class="material-symbols-outlined" aria-hidden="true">
+        play_arrow
+      </span>
+    </div>
+    <a
+      href="${root}/pages/videos/video_detail.html?id=${id}"
+      class="state-layer"
+    ></a>
+  `;
+
+  const $rippleElements = [$card, ...$card.querySelectorAll("[data-ripple]")];
+  $rippleElements.forEach(($rippleElement) => ripple($rippleElement));
+
+  const $favoriteBtn = $card.querySelector("[data-favorite-btn]");
+  favorite($favoriteBtn, "videos", id);
+
+  // hoverOnPlay($card);
+
+  return $card;
+};
