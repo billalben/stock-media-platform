e1cdb4b18527615f5727e95f5579b6c0c5e69867
Benzazoua <billalben252@gmail.com>
2024-03-26 15:41:39 +0100

    Implement masonry grid

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 // Import
 import { client } from "./api_configure.js";
 import { photoCard } from "./photo_card.js";
+import { gridInit, updateGrid } from "./utils/masonry_grid.js";
 
 // Render curated photos in home page
 const $photoGrid = document.querySelector("[data-photo-grid]");
@@ -11,8 +12,12 @@ $photoGrid.innerHTML = `<div class="skeleton"></div>`.repeat(18);
 client.photos.curated({ page: 1, per_page: 10 }, (data) => {
   $photoGrid.innerHTML = "";
 
+  const photoGrid = gridInit($photoGrid);
+
   data.photos.forEach((photo) => {
     const $photoCard = photoCard(photo);
-    $photoGrid.appendChild($photoCard);
+
+    // $photoGrid.appendChild($photoCard);
+    updateGrid($photoCard, photoGrid.$columnsHeight, photoGrid.$columns);
   });
 });
--- js/utils/masonry_grid.js
+++ js/utils/masonry_grid.js
@@ -1,1 +1,41 @@
 "use strict";
+
+/**
+ * Initial columns.
+ * @param {Node} $gridContainer - Grid container.
+ * @returns {object} - Columns & columns height array.
+ */
+
+export const gridInit = ($gridContainer) => {
+  const $columns = [];
+  const $columnsHeight = [];
+
+  const columnCount = Number(
+    getComputedStyle($gridContainer).getPropertyValue("--column-count")
+  );
+
+  for (let i = 0; i < columnCount; i++) {
+    const $column = document.createElement("div");
+    $column.classList.add("column");
+    $gridContainer.appendChild($column);
+    $columns.push($column);
+    $columnsHeight.push(0);
+  }
+
+  return { $columns, $columnsHeight };
+};
+
+/**
+ * Update masonry grid.
+ * @param {Node} $card - Grid item.
+ * @param {Array} columnsHeight - Heights of all columns.
+ * @param {NodeList} $columns - All columns.
+ */
+
+export const updateGrid = function ($card, columnsHeight, $columns) {
+  const minColumnHeight = Math.min(...columnsHeight);
+  const minColumnIndex = columnsHeight.indexOf(minColumnHeight);
+
+  $columns[minColumnIndex].appendChild($card);
+  columnsHeight[minColumnIndex] = $columns[minColumnIndex].offsetHeight;
+};
